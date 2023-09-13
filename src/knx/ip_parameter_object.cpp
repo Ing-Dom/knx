c40d98de70bd634479b3241462b4ef81515057f8
Mike <45664417+thewhobox@users.noreply.github.com>
2023-09-13 14:57:46 +0200

    wip: tunneling

--- src/knx/ip_parameter_object.cpp
+++ src/knx/ip_parameter_object.cpp
@@ -35,7 +35,7 @@ IpParameterObject::IpParameterObject(DeviceObject& deviceObject, Platform& platf
                 return 1; 
             }),
 #ifdef KNX_TUNNELING
-        new DataProperty(PID_ADDITIONAL_INDIVIDUAL_ADDRESSES, true, PDT_UNSIGNED_INT, (KNX_TUNNELING * 2) + 1, ReadLv3 | WriteLv3),
+        new DataProperty(PID_ADDITIONAL_INDIVIDUAL_ADDRESSES, true, PDT_UNSIGNED_INT, KNX_TUNNELING, ReadLv3 | WriteLv3),
 #endif
         new DataProperty(PID_CURRENT_IP_ASSIGNMENT_METHOD, false, PDT_UNSIGNED_CHAR, 0, ReadLv3 | WriteLv3),
         new DataProperty(PID_IP_ASSIGNMENT_METHOD, true, PDT_UNSIGNED_CHAR, 1, ReadLv3 | WriteLv3),
