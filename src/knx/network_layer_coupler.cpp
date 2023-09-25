75c0d5b6fcd7a2c05f93c3618ea908c767da11f6
Ing-Dom <dom@ing-dom.de>
2023-09-25 07:26:16 +0200

    minor fix for compiler warning

--- src/knx/network_layer_coupler.cpp
+++ src/knx/network_layer_coupler.cpp
@@ -108,9 +108,6 @@ bool NetworkLayerCoupler::isRoutedIndividualAddress(uint16_t individualAddress, 
     uint16_t ownAA = _deviceObj.individualAddress() & 0xF000;  // Own area address
     uint16_t ZS = individualAddress & 0xFF00;                        // destination subnetwork address (area + line)
     uint16_t Z = individualAddress & 0xF000;                         // destination area address
-    uint16_t D = individualAddress & 0x00FF;                         // destination device address (without subnetwork part)
-    uint16_t SD = individualAddress & 0x0FFF;                        // destination device address (with line part, but without area part)
-
 
 
     if (_couplerType == LineCoupler)
